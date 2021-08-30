95b300efca9e73f86eca363e09b69f4f0d619522
mmsesay <maej_01@hotmail.com>
2021-08-30 17:28:55 +0000

    Add: functionality to stop scroll when the mobile menu is show Add: functionality to take the user to the correct part of the page from the links

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ let hamburger = document.getElementById('hamburger');
 let mobile_menu = document.getElementById('mobile_menu');
 let times = document.getElementById('fa-times');
 let header = document.getElementById('header');
-/*let mobile_menu_items = document.getElementsByClassName('mobile_menu_items';)*/
+let body = document.querySelector('body');
 
 document.querySelectorAll('.mobile_menu_items').forEach((item) => {
     item.addEventListener('click', () => {
@@ -10,18 +10,17 @@ document.querySelectorAll('.mobile_menu_items').forEach((item) => {
       times.style.display = "none";
       hamburger.style.display = 'block';
     });
-  });
+});
 
 hamburger.addEventListener('click', function() {
     mobile_menu.style.display = 'block';
     times.style.display = 'block';
     hamburger.style.display = 'none';
-    /*header.style.display = 'none';*/
+    body.style.overflow = 'hidden';
 });
 
 times.addEventListener('click', function() {
     mobile_menu.style.display = 'none';
     times.style.display = 'none';
     hamburger.style.display = 'block';
-    /*header.style.display = 'none';*/
 });
