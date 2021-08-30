ead82e64bf6b569f4bc12ebadf0d66431a533dc1
Goldinium <badrbenslama8@gmail.com>
2021-08-30 19:04:15 +0100

    fix linters errors

--- script.js
+++ script.js
@@ -1,28 +1,27 @@
-let hamburger = document.getElementById('hamburger');
-let mobile_menu = document.getElementById('mobile_menu');
-let times = document.getElementById('fa-times');
-let header = document.getElementById('header');
-let body = document.querySelector('body');
+const hamburger = document.getElementById('hamburger');
+const mobileMenu = document.getElementById('mobile_menu');
+const times = document.getElementById('fa-times');
+const body = document.querySelector('body');
 
-document.querySelectorAll('.mobile_menu_items').forEach((item) => {
-    item.addEventListener('click', () => {
-      mobile_menu.style.display = 'none';
-      times.style.display = "none";
-      hamburger.style.display = 'block';
-      body.style.overflow = 'scroll';
-    });
-});
-
-hamburger.addEventListener('click', function() {
-    mobile_menu.style.display = 'block';
-    times.style.display = 'block';
-    hamburger.style.display = 'none';
-    body.style.overflow = 'hidden';
-});
-
-times.addEventListener('click', function() {
-    mobile_menu.style.display = 'none';
+document.querySelectorAll('.mobile_items').forEach((item) => {
+  item.addEventListener('click', () => {
+    mobileMenu.style.display = 'none';
     times.style.display = 'none';
     hamburger.style.display = 'block';
     body.style.overflow = 'scroll';
+  });
+});
+
+hamburger.addEventListener('click', () => {
+  mobileMenu.style.display = 'block';
+  times.style.display = 'block';
+  hamburger.style.display = 'none';
+  body.style.overflow = 'hidden';
+});
+
+times.addEventListener('click', () => {
+  mobileMenu.style.display = 'none';
+  times.style.display = 'none';
+  hamburger.style.display = 'block';
+  body.style.overflow = 'scroll';
 });
